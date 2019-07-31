419b6f2c2e5b550f259b78bbb638651b2877cb27
Rodrigo Benini <rodrigobenini15@gmail.com>
2019-07-30 22:44:32 -0300

    alteracao
    alteracao

--- index/registra_produto.php
+++ index/registra_produto.php
@@ -1,5 +1,6 @@
 <?php
 require_once('conexao_banco.php');
+require_once('cadastro_produto.php');
 //Regista dados do formulario de Produtos
 $id = $_POST['id'];
 $nome_produto = $_POST['nome_produto'];
@@ -17,11 +18,24 @@ $objetodb = new db();
 $link =  $objetodb -> conecta_mysql();
 $sql = "insert into produtos(id,nome_produto,preco_produto,descricao_produto,altura_produto,largura_produto,profundidade_produto) values( '$id','$nome_produto','$preco_produto','$descricao_produto', '$altura_produto','$largura_produto','$profundidade_produto')";
 
+$verifica_id = "select id from produtos";
+
+
 if(mysqli_query($link, $sql)){
-		echo 'Produto cadastrado com sucesso, Redirecionando para a página inicial...';
+		echo '<br>
+		<div class="alert alert-success text-center" role="alert">
+  				Produto cadastrado com sucesso, Redirecionando para a página inicial...
+			</div>';
 		header("refresh:3;url=home.php");
 	}else{
-		echo 'Erro ao cadastrar produto';
+		if($id = mysqli_query($link, $verifica_id)){
+		echo '<br>
+		<div class="alert alert-warning text-center" role="alert">
+		Produto com ID ja existente, insira outra ID do produto...
+		</div>';
+		header("refresh:2;url=cadastro_produto.php");
+
+		}else{echo 'Erro ao cadastrar produto';};
 		};
 
 
